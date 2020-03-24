247ed247678fab178cc4a294f111774ddd62ffde
Joshua Williams <jmw0426@hotmail.com>
2020-03-24 17:29:47 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -9,12 +9,12 @@ end
 end
 
 def not_safe?(speed)
-  speed < 40 ? true : false
+  speed < 40 ? true : false || speed > 60 ? true : false
 end
 
-def not_safe?(speed)
-  speed > 60 ? true : false
-end
+
+  
+
 
 	
 
